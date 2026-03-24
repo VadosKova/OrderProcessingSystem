0be6183efa74b996d7fe616f424f2eecc477714a
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-03-24 13:56:19 +0200

    Updated rabbitMQ and added Dead Letter Queue

--- shared/rabbit.js
+++ shared/rabbit.js
@@ -4,9 +4,14 @@ async function connectRabbit() {
   const connection = await amqp.connect(process.env.RABBITMQ_URL);
   const channel = await connection.createChannel();
 
-  await channel.assertExchange(process.env.EXCHANGE_NAME, "topic", {
-    durable: true,
-  });
+  const exchange = process.env.EXCHANGE_NAME;
+
+  await channel.assertExchange(exchange, "topic", { durable: true });
+
+  await channel.assertExchange("dlq", "fanout", { durable: true });
+
+  await channel.assertQueue("dead_letter_queue", { durable: true });
+  await channel.bindQueue("dead_letter_queue", "dlq", "");
 
   return channel;
 }
