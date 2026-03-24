102f77b54aac9f5466ab338b58a15ab1bbdcdb8f
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-03-24 12:59:55 +0200

    Created POST and GET method for orders

--- order-service/server.js
+++ order-service/server.js
@@ -14,3 +14,22 @@ mongoose.connect(process.env.MONGO_URI_ORDER);
 connectRabbit().then((ch) => {
   channel = ch;
 });
+
+app.post("/orders", async (req, res) => {
+  const order = await Order.create({ status: "NEW" });
+
+  channel.publish(
+    process.env.EXCHANGE_NAME,
+    "order.created",
+    Buffer.from(JSON.stringify({ orderId: order._id }))
+  );
+
+  res.json(order);
+});
+
+app.get("/orders/:id", async (req, res) => {
+  const order = await Order.findById(req.params.id);
+  res.json(order);
+});
+
+app.listen(3000, () => console.log("Order Service running"));
