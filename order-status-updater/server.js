35733d592fc6db3b74d470aa474e5e690e36eb18
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-03-24 13:29:18 +0200

    Func for tracking order status

--- order-status-updater/server.js
+++ order-status-updater/server.js
@@ -8,3 +8,29 @@ const Order = mongoose.model(
 );
 
 mongoose.connect(process.env.MONGO_URI_ORDER);
+
+async function start() {
+  const channel = await connectRabbit();
+
+  const queue = "status_queue";
+
+  await channel.assertQueue(queue);
+  await channel.bindQueue(queue, process.env.EXCHANGE_NAME, "payment.*");
+
+  channel.consume(queue, async (msg) => {
+    const { orderId } = JSON.parse(msg.content.toString());
+
+    const status =
+      msg.fields.routingKey === "payment.success"
+        ? "PAID"
+        : "CANCELLED";
+
+    await Order.findByIdAndUpdate(orderId, { status });
+
+    console.log("Order updated:", orderId, status);
+
+    channel.ack(msg);
+  });
+}
+
+start();
